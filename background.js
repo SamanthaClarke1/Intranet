189023ff1d49d53708140c8d7b200680c30a8e46
Samuel Clarke <clarkepearl44@gmail.com>
2017-09-17 21:25:35 +1000

    Update background.js

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ function setup() {
    var brect = document.getElementById("body").getBoundingClientRect();
    var canvas = createCanvas(brect.width, brect.height);
    background(66);
-   canvas.parent('sketch-holder');
+   canvas.parent('body');
   
    var noff = 0;
    for(var i = -EXTS; i < POINTSy + EXTS; i++) {
