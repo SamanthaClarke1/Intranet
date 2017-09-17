b96653b9ba01c91c15dc59a032cfd43f33727e5c
Samuel Clarke <clarkepearl44@gmail.com>
2017-09-18 00:33:59 +1000

    Update background.js

--- background.js
+++ background.js
@@ -25,7 +25,7 @@ var EXTS = 3; // generated off the edges so that it doesn't go weird
 var RAND = 0.85; // only works with not perlin / random
 var COLORSEV = 0.175; // only works with perlin
 var MOVESEV = 0.01; // only works with isAlive
-var MOVESPEED = 0.02; // only works with isAlive
+var MOVESPEED = 0.05; // only works with isAlive
 
 var hpadding = 0;
 
