58b5820011649041e1438e044169fbb2c16deb02
Samuel Clarke <clarkepearl44@gmail.com>
2017-09-18 00:22:31 +1000

    Update background.js

--- background.js
+++ background.js
@@ -25,7 +25,7 @@ var EXTS = 3; // generated off the edges so that it doesn't go weird
 var RAND = 0.85; // only works with not perlin / random
 var COLORSEV = 0.175; // only works with perlin
 var MOVESEV = 0.01; // only works with isAlive
-var MOVESPEED = 0.2; // only works with isAlive
+var MOVESPEED = 0.02; // only works with isAlive
 
 var hpadding = 0;
 
@@ -34,7 +34,7 @@ function setup() {
    if(isMullum) {
 	colorMode(HSB, 255);
    }
-   frameRate(8);
+   frameRate(30);
 
    var brect = document.getElementById("body").getBoundingClientRect();
    var canvas = createCanvas(brect.width, brect.height + hpadding);
